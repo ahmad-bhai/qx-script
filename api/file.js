3b69ad92377eb99db12ced8c7c05569c7361667b
ahmad-bhai <nasiriq768@gmail.com>
2026-08-10 00:52:41 +0500

    Clean up comments in api/file.js
    Removed comments in Hindi from the request handler.

--- api/file.js
+++ api/file.js
@@ -2,9 +2,7 @@
 const express = require('express');
 const app = express();
 
-// Har kisam ki request (GET, POST, etc.) aur har URL path ke liye
 app.use((req, res) => {
-  // HTTP status code 404 set karein aur plain text response dein
   res.status(404).setHeader('Content-Type', 'text/plain').send('Not Found!');
 });
 
